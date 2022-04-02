a672e3ab53fbbeefedcde3ba8b9aa21500e851d2
SuperCoder <supercoder@AirvonSerCoder3.fritz.box>
2022-04-03 00:14:14 +0200

    first com

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const { nav } = require("./nav.js")
+
+const PORT = 9000
+const app = express()
+
+app.set("view engine", "ejs");
+
+app.use((req, _, next) => {
+    console.log("newest request", req.method, req.url);
+    next()
+})
+
+app.use(express.static("public"))
+
+app.get('/', (_, res) => {
+    res.render("home", { nav })
+})
+
+app.get("/:name", (req, res) => {
+    const pagename = req.params.name
+
+    console.log(nav);
+    console.log("pagename", pagename);
+
+    const page = nav.find(site => site.url === "/" + pagename)
+
+    console.log("page", page);
+    console.log(page.name());
+
+    const pname = page.name()
+    res.render(pname, { nav })
+})
+
+app.listen(PORT), () => console.log("myserver is listening on port", PORT)
---
+++ nav.js
@@ -0,0 +1,21 @@
+const nav = [{
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "Team",
+        url: "/team"
+    },
+    {
+        name: "About",
+        url: "/about"
+    },
+    {
+        name: "Contact",
+        url: "/contact"
+    }
+]
+
+module.exports = {
+    nav
+}
